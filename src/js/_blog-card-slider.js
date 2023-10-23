dbc6ad9b0a4de2a10055a22349eafc7bb0cda17c
SahriarShuvro <80584229+SahriarShuvro@users.noreply.github.com>
2023-10-23 15:02:11 +0600

    Add files via upload

---
+++ src/js/_blog-card-slider.js
@@ -0,0 +1,17 @@
+$(".blog-card").slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: true,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+// Nav Bar Start
+
+import { initializeNavBar } from "./_nav-bar.js";
+
+// For a browser
+window.addEventListener("DOMContentLoaded", (event) => {
+  initializeNavBar();
+});
+
+// Nav Bar End
